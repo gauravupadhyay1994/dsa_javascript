18e87e8a5584b4ddfcb9cbcf5cea4c580e5ad8b1
Gaurav <gaurav1567@gmail.com>
2023-03-03 20:03:57 +0530

    more tree and graph data added

--- dataStructure/bst.js
+++ dataStructure/bst.js
@@ -13,7 +13,81 @@ class BinarySearchTree {
   isEmpty() {
     return this.root === null;
   }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+      return;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, node) {
+    if (root.value > node.value) {
+      if (root.left === null) {
+        root.left = node;
+      } else {
+        this.insertNode(root.left, node);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = node;
+      } else {
+        this.insertNode(root.right, node);
+      }
+    }
+  }
+
+  search(value) {
+    if (this.isEmpty()) {
+      return false;
+    } else {
+      let node = this.root;
+      while (node) {
+        if (node.value === value) {
+          return true;
+        } else {
+          if (node.value > value) {
+            node = node.left;
+          } else {
+            node = node.right;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
+  }
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
-console.log(bst.isEmpty());
+// console.log(bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.inOrder(bst.root);
+
+// console.log(bst.search(100));
